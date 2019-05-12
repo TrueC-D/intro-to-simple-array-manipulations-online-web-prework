9a50766de26592e94a6137dd54d02f14ea18dc46
Alethea Cary <aletheacary@gmail.com>
2019-05-12 09:42:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ def using_push(countries_in_western_africa, next_country)
 end
   
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhod)
-   @updated_aray = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhod)
+   @updated_aray = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhod)
    
 end
 
