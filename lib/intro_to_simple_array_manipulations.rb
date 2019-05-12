5a24aa964495362432d9540a0d427c833948cd9d
Alethea Cary <aletheacary@gmail.com>
2019-05-12 10:43:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  updated_aray = countries_in_western_africa.push(next_country)
+end
+  
+def using_unshift(array, new)
+   @updated_aray = array.unshift(new)
+   
+end
+
+def using_pop(great_hits_of_the_nineties)
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  @deleted_string = @great_hits_of_the_nineties.pop
+  
+end
+
+def pop_with_args(array)
+     array.pop(2)
+   
+end
+
+def using_shift (array)
+  @im_so_over_this_city = array.shift
+end
+
+def shift_with_args(array, x=2)
+  array.shift(x)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten (array_arg)
+  array_arg.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at (array, n)
+  array.delete_at(n)
+  
+end
