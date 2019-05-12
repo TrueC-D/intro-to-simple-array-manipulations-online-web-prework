61c8f8e030bcd823f5c7aa3a150a873ce4baa73a
Alethea Cary <aletheacary@gmail.com>
2019-05-12 09:40:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,53 @@
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  updated_aray = countries_in_western_africa.push(next_country)
+end
+  
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhod)
+   @updated_aray = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhod)
+   
+end
+
+def using_pop(great_hits_of_the_nineties)
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  @deleted_string = @great_hits_of_the_nineties.pop
+  
+end
+
+def pop_with_args()
+  
+end
+
+def using_shift
+  
+end
+
+def shift_with_args()
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
